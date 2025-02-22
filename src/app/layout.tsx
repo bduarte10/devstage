038d36e5c612a4ecd6f899e034a1df2aad484b1c
Bruno Duarte <duartebruno2208@gmail.com>
2025-02-21 21:45:08 -0300

    feat: atualiza layout da página inicial, adiciona seção de inscrição e informações sobre o evento

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,32 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Montserrat, Oxanium } from "next/font/google";
+import './globals.css'
+import type { Metadata } from 'next'
+import { Montserrat, Oxanium } from 'next/font/google'
 
 export const metadata: Metadata = {
-  title: "DevStage",
-};
+  title: 'DevStage',
+}
 
 const montserrat = Montserrat({
-  weight: ["500", "600"],
-  subsets: ["latin"],
-  variable: "--font-montserrat",
-});
+  weight: ['500', '600'],
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+})
 const oxanium = Oxanium({
-  weight: ["400", "600"],
-  subsets: ["latin"],
-  variable: "--font-oxanium",
-});
+  weight: ['400', '600'],
+  subsets: ['latin'],
+  variable: '--font-oxanium',
+})
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="pt-BR" className={`${oxanium.variable} ${montserrat.variable}`}>
-      <body className="bg-gray-900 text-gray-100 antialiased">{children}</body>
+      <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat bg-top md:bg-right-top">
+        {children}
+      </body>
     </html>
-  );
+  )
 }
